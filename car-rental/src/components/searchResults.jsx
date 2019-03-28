0d83a0f5d20000a464f713546de8f4f596aa3566
Prakhar <prakhar@farmguide.in>
2019-03-28 17:35:28 +0530

    Routing structure integration

---
+++ car-rental/src/components/searchResults.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class SearchResults extends Component {
+	render() {
+		return (
+				<div>Search Results</div>
+			)
+	}
+}
--- car-rental/src/index.js
+++ car-rental/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
+import Landing from './components/Landing';
+import SearchResults from './components/searchResults';
 
-ReactDOM.render(
-		<div>App initialisation</div>, document.getElementById('root')
-	);
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={Landing} />
+      <Route path="/search" component={SearchResults} />
+    </div>
+  </Router>
+  );
+
+ReactDOM.render(routing, document.getElementById('root'));
